359a9679d8273a73cc9b6d796cd971b3d9416802
bobo-h <bo_asapap@kakao.com>
2024-07-12 20:02:27 +0900

    feat: toggle, 수량, 금액 증가 로직 구현

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -12,6 +12,7 @@ const Payment = () => {
           <button className="w-284 h-[56px] text-white bg-[#5f0080] rounded">
             주문하기
           </button>
+          {/* {isLoggedIN ? ( */}
           <ui className="py-4 w-284 h-[180px]">
             <li className="text-xs text-[#666] pt-1 pl-1.5 list-none">
               쿠폰/적립금은 주문서에서 사용 가능합니다.
@@ -30,6 +31,16 @@ const Payment = () => {
               상품별로 적립금 지급 기준이 다를 수 있습니다.(상품 상세 페이지에서
               확인 가능합니다.)
             </li>
+            {/* ) : (
+            <ui>
+              <li>
+              [주문완료] 상태일 경우에만 주문 취소 가능합니다.
+              </li>
+              <li>
+              [마이컬리>주문내역 상세페이지]에서 직접 취소하실 수 있습니다.
+              </li>
+            </ui>
+            )} */}
           </ui>
         </div>
       </div>
--- src/components/shoppingList/ShoppingListItem.js
+++ src/components/shoppingList/ShoppingListItem.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingListItemName from "./ShoppingListItemInfo";
 import ShoppingListCheckBox from "./ShoppingListCheckBox";
 
 const ShoppingListItem = ({ isChecked, onCheckboxChange }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [isDetailsVisible, setIsDetailsVisible] = useState(true);
+  const pricePerItem = 11890; // Example price per item
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  // Calculate total cost based on quantity
+  const totalCost = quantity * pricePerItem;
+
+  // Toggle the visibility of the details and change the icon
+  const toggleDetails = () => {
+    setIsDetailsVisible(!isDetailsVisible);
+  };
+
   return (
     <div>
       <div
@@ -15,100 +37,129 @@ const ShoppingListItem = ({ isChecked, onCheckboxChange }) => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="size-7"
+              className="size-7"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
               />
             </svg>
           </div>
           <div className="font-semibold text-[17px]">냉동 상품</div>
         </div>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m4.5 15.75 7.5-7.5 7.5 7.5"
-            />
-          </svg>
+        <div onClick={toggleDetails} className="cursor-pointer">
+          {isDetailsVisible ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 15.75 7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          )}
         </div>
       </div>
 
-      <div
-        className="py-5 pl-0.5 h-28 flex items-center"
-        style={{ borderBottom: "1px solid #f7f7f7" }}
-      >
-        <label htmlFor="checkbox" className="cursor-pointer flex items-center">
-          <ShoppingListCheckBox
-            isChecked={isChecked}
-            onCheckboxChange={onCheckboxChange}
-          />
-        </label>
-        <ShoppingListItemName />
+      {isDetailsVisible && (
+        <div
+          className="py-5 pl-0.5 h-28 flex items-center"
+          style={{ borderBottom: "1px solid #f7f7f7" }}
+        >
+          <label
+            htmlFor="checkbox"
+            className="cursor-pointer flex items-center"
+          >
+            <ShoppingListCheckBox
+              isChecked={isChecked}
+              onCheckboxChange={onCheckboxChange}
+            />
+          </label>
 
-        <div className="flex" style={{ border: "1px solid #e2e3e4" }}>
-          <button className="w-7 h-7 flex justify-center items-center">
+          <ShoppingListItemName />
+
+          <div className="flex" style={{ border: "1px solid #e2e3e4" }}>
+            <button
+              className="w-7 h-7 flex justify-center items-center"
+              onClick={decrementQuantity}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="size-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+
+            <div className="w-7 h-7 text-sm flex justify-center items-center">
+              {quantity}
+            </div>
+
+            <button
+              className="w-7 h-7 flex justify-center items-center"
+              onClick={incrementQuantity}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="size-5"
+              >
+                <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+              </svg>
+            </button>
+          </div>
+          <div className="w-[7.9375rem] text-right text-base font-bold">
+            {totalCost.toLocaleString()} {/* Display total cost with commas */}
+          </div>
+          <button className="w-[1.875rem] h-[1.875rem] ml-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="size-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-5"
             >
               <path
-                fill-rule="evenodd"
-                d="M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z"
-                clip-rule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
               />
             </svg>
           </button>
-
-          <div className="w-7 h-7 text-sm flex justify-center items-center">
-            1
-          </div>
-
-          <button className="w-7 h-7 flex justify-center items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="size-5"
-            >
-              <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-            </svg>
-          </button>
         </div>
-        <div className="w-[7.9375rem] text-right text-base font-bold">
-          11,890
-        </div>
-        <button className="w-[1.875rem] h-[1.875rem] ml-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
+      )}
     </div>
   );
 };
