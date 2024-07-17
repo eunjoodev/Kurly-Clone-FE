c60c4e1cba7c93991bad960f6e4139cca71fdf51
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-07-17 12:40:53 +0900

    feat: 로그인 연동 준비 로직 수정 및 주문 설명 UI 수정

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -16,34 +16,34 @@ const Payment = ({ cartDetails }) => {
             주문하기
           </button>
           <ul className="py-4 w-284 h-[180px] list-none text-[#666]">
-            <li className="text-xs pt-1 pl-1.5">
+            <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
               쿠폰/적립금은 주문서에서 사용 가능합니다.
             </li>
-            <li className="text-xs pt-1 pl-1.5">
+            <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
               [주문완료]상태일 경우에만 주문 취소 가능합니다.
             </li>
-            <li className="text-xs pt-1 pl-1.5">
-              [마이컬리 주문내역 상세페이지]에서 직접 취소하실 수 있습니다.
+            <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
+              [마이컬리 &gt; 주문내역 상세페이지]에서 직접 취소하실 수 있습니다.
             </li>
-            <li className="text-xs pt-1 pl-1.5">
+            <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
               쿠폰, 적립금 사용 금액을 제외한 실 결제 금액 기준으로 최종
               산정됩니다.
             </li>
-            <li className="text-xs pt-1 pl-1.5">
+            <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
               상품별로 적립금 지급 기준이 다를 수 있습니다.(상품 상세 페이지에서
               확인 가능합니다.)
             </li>
           </ul>
           {/* ) : (
-            <button className="w-284 h-[56px] text-white bg-[#5f0080] rounded">
+            <a href="./login" className="w-284 h-[56px] text-white bg-[#5f0080] rounded cursor-pointer">
               로그인
-            </button>
-            <ul>
-              <li>
+            </a>
+            <ul className="py-4 w-284 h-[180px] list-none text-[#666]">
+              <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
               [주문완료] 상태일 경우에만 주문 취소 가능합니다.
               </li>
-              <li>
-              [마이컬리>주문내역 상세페이지]에서 직접 취소하실 수 있습니다.
+              <li className="text-xs pt-1 pl-1.5 before:overflow-hidden before:absolute before:w-px before:h-px before:mt-1.5 before:-ml-1.5 before:bg-[#999999] before:align-top before:rounded-[50%] before:content-['']">
+              [마이컬리 &gt; 주문내역 상세페이지]에서 직접 취소하실 수 있습니다.
               </li>
             </ul>
             )} */}
