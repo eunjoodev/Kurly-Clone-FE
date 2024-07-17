49ee906d00fc2a45aade3faa41eff506b4eb58fa
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-07-17 13:12:06 +0900

    feat: line-hight 수정

--- src/components/payment/PaymentAmount.js
+++ src/components/payment/PaymentAmount.js
@@ -14,19 +14,19 @@ const PaymentAmount = ({ cartDetails }) => {
 
   return (
     <div className="p-5 bg-neutral-50" style={{ border: "1px solid #f5f5f5" }}>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center h-6">
         <div className="text-base">상품금액</div>
-        <div className="text-lg">{totalPrice.toLocaleString()}원</div>
+        <div className="text-lg leading-6">{totalPrice.toLocaleString()}원</div>
       </div>
       <div className="flex justify-between items-center pt-3">
         <div className="text-base">상품할인금액</div>
-        <div className="text-lg">
+        <div className="text-lg leading-6">
           {totalDiscount === 0 ? "0원" : `-${totalDiscount.toLocaleString()}원`}
         </div>
       </div>
       <div className="flex justify-between items-center pt-3">
         <div className="text-base">배송비</div>
-        <div className="text-lg">
+        <div className="text-lg leading-6">
           {shippingCost === 0 ? "0원" : `+${shippingCost.toLocaleString()}원`}
         </div>
       </div>
